504449d8fd8dad5f6bbf2edf55c709a84de90ef8
Future Inspire Games <shunsei.chan@gmail.com>
2025-11-09 06:30:04 -0600

    added enemy health component

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int Health = 10;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
